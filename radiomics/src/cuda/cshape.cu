ebd1b9e89f9a1c466eb59bee765f9eb1c6802049
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-04-01 11:18:23 +0200

    Another adjustments

--- radiomics/src/cuda/cshape.cu
+++ radiomics/src/cuda/cshape.cu
@@ -1,23 +1,26 @@
+#include "test.cuh"
 #include <stdlib.h>
 
-/* Pick the best solution here */
-extern int calculate_coefficients_cuda_1(char *mask, int *size, int *strides, double *spacing,
-                   double *surfaceArea, double *volume, double *diameters);
+/* This file stands as interface to the CUDA code from pyradiomics library */
 
-int cuda_calculate_coefficients(char *mask, int *size, int *strides, double *spacing,
+/* Pick the best solution here */
+SOLUTION_DECL(0);
+
+int calculate_coefficients(char *mask, int *size, int *strides, double *spacing,
                            double *surfaceArea, double *volume, double *diameters) {
-  return calculate_coefficients_cuda_1(
-      mask,
-      size,
-      strides,
-      spacing,
-      surfaceArea,
-      volume,
-      diameters
-  );
+    return SOLUTION_NAME(0)(
+        mask,
+        size,
+        strides,
+        spacing,
+        surfaceArea,
+        volume,
+        diameters
+    );
 }
 
-int cuda_calculate_coefficients2D(char *mask, int *size, int *strides, double *spacing,
+int calculate_coefficients2D(char *mask, int *size, int *strides, double *spacing,
                              double *perimeter, double *surface, double *diameter) {
-  exit(EXIT_FAILURE);
+    // TODO: Attach own implementation or reuse
+    exit(EXIT_FAILURE);
 }
