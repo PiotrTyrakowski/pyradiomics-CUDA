f5e4b15a3025cd147a5b8ef511805e7481700348
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-04-03 19:39:32 +0200

    Init rework

--- radiomics/src/cuda/test.cu
+++ radiomics/src/cuda/test.cu
@@ -41,7 +41,9 @@ int AddShape2DFunction(size_t idx, shape_2D_func_t func) {
 }
 
 SOLUTION_DECL(0);
+SOLUTION_DECL(1);
 
 void RegisterSolutions() {
     REGISTER_SOLUTION(0);
+    REGISTER_SOLUTION(1);
 }
